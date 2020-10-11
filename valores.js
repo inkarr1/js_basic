87aedcb2729adb69654ac779d89010c350981554
Jhanpiere Montes <jhanpieremontes@gmail.com>
2020-10-10 20:40:21 -0500

    Este es el primer commit sobre el curso de JavaScript

---
+++ valores.js
@@ -0,0 +1,13 @@
+40
+
+"Jhanpiere Montes"
+
+true
+false
+
+null
+undefined
+
+//
+[1,2,3]
+{ nombre: "Diego"}
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Jhanpiere";
+
+//declarar
+var edad;
+//inicializar
+edad = 30;
+
+edad
+
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Jhanpiere",
+    edad: 30
+}
+
+persona;
